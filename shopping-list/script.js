bffe35285c6744d980d5ca50799cc8b039a74f53
alexanderkolosov <avk7961555@gmail.com>
2024-09-03 18:26:56 +0300

    Your commit message here

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const inputForm = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(evt) {
     evt.preventDefault();
@@ -17,7 +20,10 @@ function addItem(evt) {
    const button = createButton('remove-item btn-link text-red');
    li.appendChild(button);
 
+   // Add li to the DOM
    itemList.appendChild(li);
+
+   checkUI();
 
    inputForm.value = '';
 
@@ -39,6 +45,39 @@ function createIcon(classes) {
     return icon;
 }
 
+// this is an action which allows us to REMOVE item from a list with using X 
+
+function removeItem (evt) {
+    if (evt.target.parentElement.classList.contains('remove-item')){
+        if (confirm('Are you sure?')) { //CONFIRMATION OPTION
+        evt.target.parentElement.parentElement.remove();
+
+        checkUI();
+        };
+    };
+}
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
 //Event listener
 
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', addItem); // this event activate additem when the button is submitted 
+itemList.addEventListener('click', removeItem); //This event activate function when the red cross is clicked
+clearButton.addEventListener('click', clearItems);
+
+checkUI();
