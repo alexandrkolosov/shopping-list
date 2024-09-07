b3a038846ec432da60554091701e818c0c6a9546
alexanderkolosov <avk7961555@gmail.com>
2024-09-07 17:56:56 +0300

    update items added function

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -3,6 +3,8 @@ const inputForm = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false; 
 
 
 function displayItems() {
@@ -93,17 +95,50 @@ function getItemsFromStorage() {
     return itemsFromStorage;
 
 }
+//Remove from local storage
 
+function onClickItem(evt) {
+    if (evt.target.parentElement.classList.contains('remove-item')) {
+        removeItem(evt.target.parentElement.parentElement);
+    } else {
+        setItemToEdit(evt.target);
+    }
+
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+    itemList.querySelectorAll('li').forEach(y => y.classList.remove('edit-mode'));
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.style.backgroundColor = '#228B22'
+    inputForm.value = item.textContent;
+}
 // this is an action which allows us to REMOVE item from a list with using X 
 
-function removeItem (evt) {
-    if (evt.target.parentElement.classList.contains('remove-item')){
-        if (confirm('Are you sure?')) { //CONFIRMATION OPTION
-        evt.target.parentElement.parentElement.remove();
+function removeItemFromStorage(item) {
+    let itemsFromStorage = getItemsFromStorage();
+
+    //filter out item to be removed 
+
+    itemsFromStorage = itemsFromStorage.filter((y) => y != item);
+
+    //Re-set to local storage 
+
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function removeItem (item) {
+    if (confirm('Are you sure?')) {
+        //remove from DOM
+        item.remove();
+
+        //Remove from Storage
+        removeItemFromStorage(item.textContent);
 
         checkUI();
-        };
-    };
+    }
+ 
 }
 
 function filterItems(evt) {
@@ -122,8 +157,12 @@ function filterItems(evt) {
 function clearItems() {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
+    }
 
-        checkUI();
+    //clear from local storage
+    if (confirm('Are you to delete it ALL?')) {
+    localStorage.removeItem('items');
+    checkUI();
     }
 }
 
@@ -142,7 +181,7 @@ function init () {
 //Event listener
 
 itemForm.addEventListener('submit', onAddItemSubmit); // this event activate additem when the button is submitted 
-itemList.addEventListener('click', removeItem); //This event activate function when the red cross is clicked
+itemList.addEventListener('click', onClickItem); //This event activate function when the red cross is clicked
 clearButton.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);
 document.addEventListener('DOMContentLoaded', displayItems);
